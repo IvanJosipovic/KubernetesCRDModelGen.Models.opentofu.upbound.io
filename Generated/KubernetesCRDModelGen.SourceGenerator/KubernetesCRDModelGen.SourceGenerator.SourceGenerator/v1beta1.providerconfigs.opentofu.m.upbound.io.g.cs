6d4b33282a4791a3c644a6c4a026244541744299
IvanJosipovic <9521987+IvanJosipovic@users.noreply.github.com>
2026-04-25 08:19:13 +0000

    feat: Update CRDs

---
+++ Generated/KubernetesCRDModelGen.SourceGenerator/KubernetesCRDModelGen.SourceGenerator.SourceGenerator/v1beta1.providerconfigs.opentofu.m.upbound.io.g.cs
@@ -0,0 +1,259 @@
+﻿#nullable enable
+using k8s;
+using k8s.Models;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+namespace KubernetesCRDModelGen.Models.opentofu.m.upbound.io;
+/// <summary>A ProviderConfig configures the OpenTofu provider.</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.5.2+8c7b4a3647c1e77cd3e3152af5701ec2357dafe9")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+[KubernetesEntity(Group = KubeGroup, Kind = KubeKind, ApiVersion = KubeApiVersion, PluralName = KubePluralName)]
+public partial class V1beta1ProviderConfigList : IKubernetesObject<V1ListMeta>, IItems<V1beta1ProviderConfig>
+{
+    public const string KubeApiVersion = "v1beta1";
+    public const string KubeKind = "ProviderConfigList";
+    public const string KubeGroup = "opentofu.m.upbound.io";
+    public const string KubePluralName = "providerconfigs";
+    /// <summary>APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources</summary>
+    [JsonPropertyName("apiVersion")]
+    public string ApiVersion { get; set; } = "opentofu.m.upbound.io/v1beta1";
+
+    /// <summary>Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds</summary>
+    [JsonPropertyName("kind")]
+    public string Kind { get; set; } = "ProviderConfigList";
+
+    /// <summary>ListMeta describes metadata that synthetic resources must have, including lists and various status objects. A resource may have only one of {ObjectMeta, ListMeta}.</summary>
+    [JsonPropertyName("metadata")]
+    public V1ListMeta? Metadata { get; set; }
+
+    /// <summary>List of V1beta1ProviderConfig objects.</summary>
+    [JsonPropertyName("items")]
+    public IList<V1beta1ProviderConfig>? Items { get; set; }
+}
+
+/// <summary>
+/// Env is a reference to an environment variable that contains credentials
+/// that must be used to connect to the provider.
+/// </summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.5.2+8c7b4a3647c1e77cd3e3152af5701ec2357dafe9")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1beta1ProviderConfigSpecCredentialsEnv
+{
+    /// <summary>Name is the name of an environment variable.</summary>
+    [JsonPropertyName("name")]
+    public required string Name { get; set; }
+}
+
+/// <summary>
+/// Fs is a reference to a filesystem location that contains credentials that
+/// must be used to connect to the provider.
+/// </summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.5.2+8c7b4a3647c1e77cd3e3152af5701ec2357dafe9")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1beta1ProviderConfigSpecCredentialsFs
+{
+    /// <summary>Path is a filesystem path.</summary>
+    [JsonPropertyName("path")]
+    public required string Path { get; set; }
+}
+
+/// <summary>
+/// A SecretRef is a reference to a secret key that contains the credentials
+/// that must be used to connect to the provider.
+/// </summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.5.2+8c7b4a3647c1e77cd3e3152af5701ec2357dafe9")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1beta1ProviderConfigSpecCredentialsSecretRef
+{
+    /// <summary>The key to select.</summary>
+    [JsonPropertyName("key")]
+    public required string Key { get; set; }
+
+    /// <summary>Name of the secret.</summary>
+    [JsonPropertyName("name")]
+    public required string Name { get; set; }
+
+    /// <summary>Namespace of the secret.</summary>
+    [JsonPropertyName("namespace")]
+    public required string Namespace { get; set; }
+}
+
+/// <summary>Source of the provider credentials.</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.5.2+8c7b4a3647c1e77cd3e3152af5701ec2357dafe9")]
+[JsonConverter(typeof(JsonStringEnumConverter<V1beta1ProviderConfigSpecCredentialsSourceEnum>))]
+public enum V1beta1ProviderConfigSpecCredentialsSourceEnum
+{
+    [EnumMember(Value = "None"), JsonStringEnumMemberName("None")]
+    None,
+    [EnumMember(Value = "Secret"), JsonStringEnumMemberName("Secret")]
+    Secret,
+    [EnumMember(Value = "Environment"), JsonStringEnumMemberName("Environment")]
+    Environment,
+    [EnumMember(Value = "Filesystem"), JsonStringEnumMemberName("Filesystem")]
+    Filesystem
+}
+
+/// <summary>ProviderCredentials required to authenticate.</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.5.2+8c7b4a3647c1e77cd3e3152af5701ec2357dafe9")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1beta1ProviderConfigSpecCredentials
+{
+    /// <summary>
+    /// Env is a reference to an environment variable that contains credentials
+    /// that must be used to connect to the provider.
+    /// </summary>
+    [JsonPropertyName("env")]
+    public V1beta1ProviderConfigSpecCredentialsEnv? Env { get; set; }
+
+    /// <summary>
+    /// Filename (relative to main.tf) to which these provider credentials
+    /// should be written.
+    /// </summary>
+    [JsonPropertyName("filename")]
+    public required string Filename { get; set; }
+
+    /// <summary>
+    /// Fs is a reference to a filesystem location that contains credentials that
+    /// must be used to connect to the provider.
+    /// </summary>
+    [JsonPropertyName("fs")]
+    public V1beta1ProviderConfigSpecCredentialsFs? Fs { get; set; }
+
+    /// <summary>
+    /// A SecretRef is a reference to a secret key that contains the credentials
+    /// that must be used to connect to the provider.
+    /// </summary>
+    [JsonPropertyName("secretRef")]
+    public V1beta1ProviderConfigSpecCredentialsSecretRef? SecretRef { get; set; }
+
+    /// <summary>Source of the provider credentials.</summary>
+    [JsonPropertyName("source")]
+    public required V1beta1ProviderConfigSpecCredentialsSourceEnum Source { get; set; }
+}
+
+/// <summary>A ProviderConfigSpec defines the desired state of a ProviderConfig.</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.5.2+8c7b4a3647c1e77cd3e3152af5701ec2357dafe9")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1beta1ProviderConfigSpec
+{
+    /// <summary>
+    /// Tofu backend file configuration content,
+    /// it has the contents of the backend block as top-level attributes,
+    /// without the need to wrap it in another opentofu or backend block.
+    /// More details at https://opentofu.org/docs/language/settings/backends/configuration/#file.
+    /// </summary>
+    [JsonPropertyName("backendFile")]
+    public string? BackendFile { get; set; }
+
+    /// <summary>
+    /// Configuration that should be injected into all workspaces that use
+    /// this provider config, expressed as inline HCL. This can be used to
+    /// automatically inject Terraform provider configuration blocks.
+    /// </summary>
+    [JsonPropertyName("configuration")]
+    public string? Configuration { get; set; }
+
+    /// <summary>Credentials required to authenticate to this provider.</summary>
+    [JsonPropertyName("credentials")]
+    public IList<V1beta1ProviderConfigSpecCredentials>? Credentials { get; set; }
+
+    /// <summary>
+    /// PluginCache enables tofu provider plugin caching mechanism
+    /// https://opentofu.org/docs/cli/config/config-file/#provider-plugin-cache
+    /// </summary>
+    [JsonPropertyName("pluginCache")]
+    public bool? PluginCache { get; set; }
+}
+
+/// <summary>A Condition that may apply to a resource.</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.5.2+8c7b4a3647c1e77cd3e3152af5701ec2357dafe9")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1beta1ProviderConfigStatusConditions
+{
+    /// <summary>
+    /// LastTransitionTime is the last time this condition transitioned from one
+    /// status to another.
+    /// </summary>
+    [JsonPropertyName("lastTransitionTime")]
+    public required DateTime LastTransitionTime { get; set; }
+
+    /// <summary>
+    /// A Message containing details about this condition&apos;s last transition from
+    /// one status to another, if any.
+    /// </summary>
+    [JsonPropertyName("message")]
+    public string? Message { get; set; }
+
+    /// <summary>
+    /// ObservedGeneration represents the .metadata.generation that the condition was set based upon.
+    /// For instance, if .metadata.generation is currently 12, but the .status.conditions[x].observedGeneration is 9, the condition is out of date
+    /// with respect to the current state of the instance.
+    /// </summary>
+    [JsonPropertyName("observedGeneration")]
+    public long? ObservedGeneration { get; set; }
+
+    /// <summary>A Reason for this condition&apos;s last transition from one status to another.</summary>
+    [JsonPropertyName("reason")]
+    public required string Reason { get; set; }
+
+    /// <summary>Status of this condition; is it currently True, False, or Unknown?</summary>
+    [JsonPropertyName("status")]
+    public required string Status { get; set; }
+
+    /// <summary>
+    /// Type of this condition. At most one of each condition type may apply to
+    /// a resource at any point in time.
+    /// </summary>
+    [JsonPropertyName("type")]
+    public required string Type { get; set; }
+}
+
+/// <summary>A ProviderConfigStatus reflects the observed state of a ProviderConfig.</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.5.2+8c7b4a3647c1e77cd3e3152af5701ec2357dafe9")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1beta1ProviderConfigStatus
+{
+    /// <summary>Conditions of the resource.</summary>
+    [JsonPropertyName("conditions")]
+    public IList<V1beta1ProviderConfigStatusConditions>? Conditions { get; set; }
+
+    /// <summary>Users of this provider configuration.</summary>
+    [JsonPropertyName("users")]
+    public long? Users { get; set; }
+}
+
+/// <summary>A ProviderConfig configures the OpenTofu provider.</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.5.2+8c7b4a3647c1e77cd3e3152af5701ec2357dafe9")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+[KubernetesEntity(Group = KubeGroup, Kind = KubeKind, ApiVersion = KubeApiVersion, PluralName = KubePluralName)]
+public partial class V1beta1ProviderConfig : IKubernetesObject<V1ObjectMeta>, ISpec<V1beta1ProviderConfigSpec>, IStatus<V1beta1ProviderConfigStatus?>
+{
+    public const string KubeApiVersion = "v1beta1";
+    public const string KubeKind = "ProviderConfig";
+    public const string KubeGroup = "opentofu.m.upbound.io";
+    public const string KubePluralName = "providerconfigs";
+    /// <summary>APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources</summary>
+    [JsonPropertyName("apiVersion")]
+    public string ApiVersion { get; set; } = "opentofu.m.upbound.io/v1beta1";
+
+    /// <summary>Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds</summary>
+    [JsonPropertyName("kind")]
+    public string Kind { get; set; } = "ProviderConfig";
+
+    /// <summary>Standard object&apos;s metadata. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#metadata</summary>
+    [JsonPropertyName("metadata")]
+    public V1ObjectMeta Metadata { get; set; }
+
+    /// <summary>A ProviderConfigSpec defines the desired state of a ProviderConfig.</summary>
+    [JsonPropertyName("spec")]
+    public required V1beta1ProviderConfigSpec Spec { get; set; }
+
+    /// <summary>A ProviderConfigStatus reflects the observed state of a ProviderConfig.</summary>
+    [JsonPropertyName("status")]
+    public V1beta1ProviderConfigStatus? Status { get; set; }
+}
